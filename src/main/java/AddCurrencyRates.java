ab80a26ec86d2ff8942c81a0b11218d9d6925452
LTZ <alanlin_yu@hotmail.com>
2025-02-18 21:57:52 +0800

    resolve sonarqube findings

--- src/main/java/AddCurrencyRates.java
+++ src/main/java/AddCurrencyRates.java
@@ -26,14 +26,14 @@ public class AddCurrencyRates extends HttpServlet {
      */
     public AddCurrencyRates() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
